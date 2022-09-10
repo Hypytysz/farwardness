7ae3a9db27a49d2b427ab84972d181b536ab2df6
Hypytysz <arek1nowosielski@gmail.com>
2022-09-10 15:03:13 +0200

    Test

--- forwardness/urls.py
+++ forwardness/urls.py
@@ -17,8 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import  static
-
+from rest_framework import routers
 from registration import views
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
